49cf94ab2fe2205ab8c0a359059f0b13d0b3792a
seda-karadeniz <sedayo98@gmail.com>
2020-12-03 13:29:29 +0100

    completed

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+
+const app = {
+    init(){
+        this.burgerMenu = document.querySelector('.nav-button');
+        this.leMain = document.querySelector('.main');
+        document.documentElement.classList.add('js-enabled');
+        this.burgerMenu.addEventListener('click', () =>{this.f_burger()} );
+
+    },
+    f_burger(){
+        /* nav.classList.toggle("visible"); */
+        
+        /* if (leMain.classList.contains('is-opened')) {
+            leMain.classList.remove('is-opened');
+        }
+        else{
+            leMain.classList.add('is-opened');
+        } */
+        console.log(this);
+        this.leMain.classList.toggle('is-opened');
+    }
+
+}
+app.init();
